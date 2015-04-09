44dc301c48d51935253ed8ffe8f89a6100593476
jose guillermo <guillox22@gmail.com>
2015-04-09 11:27:02 -0300

    tp2cej2 v0.1

---
+++ Practica/2014/tp2-continuacion/tp2cej2.py
@@ -0,0 +1,10 @@
+"""
+2.­ Escriba una función que permita realizar un merge entre los dos archivos generados en el                                
+ejercicio 1. Los nombres de los archivos vienen dados como argumento en la línea de                              
+comando.
+"""
+
+import tp2ce1
+
+tp2ce1.
+

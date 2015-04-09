732110c7dc8e054201f493cc019d2a8ece8cf46c
jose guillermo <guillox22@gmail.com>
2015-04-09 02:24:45 -0300

    tp1ej15vii v0.1

---
+++ Practica/2014/tp1/tp1ej15vii.py
@@ -0,0 +1,10 @@
+"""
+15.- Diccionarios
+b) Sea el siguiente diccionario:
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+En donde la clave identifica a la edad y el valor al nombre
+VII. Agregue a Pedro con una edad de 44 años
+"""
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+edades[44]='Pedro'
+print edades.items()

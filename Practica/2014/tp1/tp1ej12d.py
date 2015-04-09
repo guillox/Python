959b4db249f2e286089c2b71502594ce2c0672dd
jose guillermo <guillox22@gmail.com>
2015-04-09 02:14:27 -0300

    tp1ej12d v0.1

---
+++ Practica/2014/tp1/tp1ej12d.py
@@ -0,0 +1,14 @@
+""" 
+
+
+Listas, Tuplas y Diccionarios
+12.- Listas
+Dada la siguiente lista:
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+d) Imprima los elementos en las posiciones 0, 1 y 2
+
+
+
+"""
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+print lista[0:3]

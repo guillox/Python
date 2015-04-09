62428396700bb3cbad1d7f39b7d92fe0a6c99ada
jose guillermo <guillox22@gmail.com>
2015-04-09 02:13:57 -0300

    tp1ej12c v0.1

---
+++ Practica/2014/tp1/tp1ej12c.py
@@ -0,0 +1,11 @@
+"""Listas, Tuplas y Diccionarios
+12.- Listas
+Dada la siguiente lista:
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+
+
+c) Imprima todos los elementos"""
+
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+print lista[:]
+print lista[0:4]

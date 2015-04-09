2fc2eb2ecaf39c32b45f33982be6524aca7443c6
jose guillermo <guillox22@gmail.com>
2015-04-09 02:12:07 -0300

    tp1ej11b v0.1

---
+++ Practica/2014/tp1/tp1ej11b.py
@@ -0,0 +1,10 @@
+""" b) Suponga que tengo la variable numero_flotante=7.55689245 y quiero que se impriman sólo el número con 5 dígitos, 
+¿cómo debería formular la instrucción print ?
+>>> round(2.675, 2)
+2.67
+
+
+"""
+
+numero_flotante=7.55689245
+print round(numero_flotante,5)

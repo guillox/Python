0fde28bfeb692cbfff8ed6ba0c75b53811fb4a21
jose guillermo <guillox22@gmail.com>
2015-04-09 02:11:00 -0300

    tp1ej10b v0.1
    sin script

---
+++ Practica/2014/tp1/tp1ej10b(sin script).py
@@ -0,0 +1,7 @@
+"""b) Haga la inversa de a), reciba un número y devuelva a que carácter corresponde ese                              
+código ASCII
+>>> chr(65)
+'A'
+"""
+num=input('escriba un numero y pulse enter')
+print chr (num)

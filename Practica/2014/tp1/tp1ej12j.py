20895741c1cf1dcd17113a7ae14cfed7916306b4
jose guillermo <guillox22@gmail.com>
2015-04-09 02:18:12 -0300

    tp1ej12j v0.1

---
+++ Practica/2014/tp1/tp1ej12j.py
@@ -0,0 +1,10 @@
+"""
+Listas, Tuplas y Diccionarios
+12.- Listas
+Dada la siguiente lista:
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+j) Retorne el índice que tiene el elemento  con el valor: 'elemento  4'
+"""
+
+lista = [ 'elemento1' , 2 ,'elemento 3', 'elemento 4' ]
+print lista.index('elemento 4')

8a3a8771b0fde215c23f00bf2d82c01890709001
jose guillermo <guillox22@gmail.com>
2015-04-09 12:16:21 -0300

    tp1ej09 v0.1
    2015(python 2.X)/practica 1/ejercicio 9

---
+++ Practica/2015/tp1/tp1ej09.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+__author__ = 'Guillermo Marti'
+"""9. Escriba un código Python cuya salida sea lo siguiente:
+=============
+a     b     c
+d     e     f
+=============
+¿Puede hacerlo en una única línea de código?"""
+
+print '='*13
+print 'a' + ' '*5 + 'b' + ' '*5 + 'c'
+print 'd' + ' '*5 + 'e' + ' '*5 + 'f'
+print '='*13
+
+#realizado con una unica linea de codigo
+print '='*13 + '\na' + ' '*5 + 'b' + ' '*5 + 'c' + '\nd' + ' '*5 + 'e' + ' '*5 + 'f\n' + '='*13
+

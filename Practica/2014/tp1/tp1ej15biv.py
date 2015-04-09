e98af05a9346838de769ecd9c3cff8d8e2c96090
jose guillermo <guillox22@gmail.com>
2015-04-09 02:23:24 -0300

    tp1ej15biv v0.1

---
+++ Practica/2014/tp1/tp1ej15biv.py
@@ -0,0 +1,22 @@
+"""15.- Diccionarios
+b) Sea el siguiente diccionario:
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+En donde la clave identifica a la edad y el valor al nombre
+IV. Imprimir todas las personas del diccionario
+
+ejemplo
+values() Devuelve una lista de los valores del diccionario.
+
+python 2.x
+
+>>> diccionario.values()
+[2, 3, 2]
+
+python 3.x
+
+>>> list(diccionario.values())
+[2, 3, 2]
+"""
+
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+print edades.values()

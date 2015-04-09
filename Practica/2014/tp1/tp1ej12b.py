ff1f664380e1f9757dc0cfb8b90dc3b7dd6145ea
jose guillermo <guillox22@gmail.com>
2015-04-09 02:13:38 -0300

    tp1ej12b v0.1

---
+++ Practica/2014/tp1/tp1ej12b.py
@@ -0,0 +1,8 @@
+"""Listas, Tuplas y Diccionarios
+12.- Listas
+Dada la siguiente lista:
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+b) Que retorna l i s t a [ - 1 ] """
+
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+print lista[-1]

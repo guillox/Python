6dc461c714b61f344a120615486a4e203f6d022e
jose guillermo <guillox22@gmail.com>
2015-04-09 11:09:09 -0300

    tp2ej5a v0.1

---
+++ Practica/2014/tp2/tp2ej5a.py
@@ -0,0 +1,31 @@
+"""
+5.- a) Dada una lista con N números, modifique dicha lista multiplicando por 2 cada uno de los elementos. Es
+decir, si tengo [2, 3, 4, 5, 6] el resultado sería [4, 6, 8, 10, 12].
+len() tamaño de la lista
+"""
+
+lista = [2, 3, 4, 5, 6]
+for i in range(0, len(lista)):
+    lista[i] *= 2  # igual a lista[i]=lista*2
+print(lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

e87227f1cc198ba31ab4e43b0074913e18b33c45
jose guillermo <guillox22@gmail.com>
2015-04-09 02:08:37 -0300

    tp1ej8 v0.1

---
+++ Practica/2014/tp1/tp1ej8.py
@@ -0,0 +1,11 @@
+#8.- a) Modifique el ejercicio 6) para que los valores a ser sumados sean datos ingresados 
+
+#por el usuario, indicando en la pantalla que debe ingresar un número por teclado y luego otro.
+
+#b) ¿El resultado fue el esperado? ¿Qué debería modificar para que dé lo esperado (suma de números)?
+
+
+
+num1=raw_input('ingrese el numero 1')
+num2=raw_input('ingrese el numero 2')
+print num1+num2

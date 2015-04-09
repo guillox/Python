7c3a1601417c9881b33052bb067b88855287a620
jose guillermo <guillox22@gmail.com>
2015-04-09 02:45:58 -0300

    tp2ej4a v0.1

---
+++ Practica/2014/tp2/tp2ej4a.py
@@ -0,0 +1,13 @@
+"""
+4.- a) Describir qué hacen las siguientes sentencias:
+for i in range(1, 10):
+	print (i)
+else:
+	print ('Termino el bucle')
+
+"""
+#imprime i del 1 al 9 y cuando sale del for va por el else e imprime termino bucle
+for i in range(1, 10):
+    print(i)
+else:
+    print('Termino el bucle')

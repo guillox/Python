b1545baa6b8ba3c1f8cb4d07e25ada82e5224130
jose guillermo <guillox22@gmail.com>
2015-04-09 02:11:48 -0300

    tp1ej11a v0.1

---
+++ Practica/2014/tp1/tp1ej11a.py
@@ -0,0 +1,6 @@
+"""11.-  Salida estándar – p r i n t
+a) Explique qué hace la siguiente línea de código:
+print  ( "%15s   %s   Python "   %   ( " Seminario " ,   " de " ) )
+agrega Python al final de Seminario de ya que cuenta 15 espacios"""
+
+print  ( "%15s  %s  Python"   %   ( " Seminario " , " de " ) )

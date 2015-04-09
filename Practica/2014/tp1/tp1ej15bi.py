0be2377491b646080a2234a7c69e1274d6a7499e
jose guillermo <guillox22@gmail.com>
2015-04-09 02:21:32 -0300

    tp1ej15bi v0.1

---
+++ Practica/2014/tp1/tp1ej15bi.py
@@ -0,0 +1,10 @@
+"""15.- Diccionarios
+b) Sea el siguiente diccionario:
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+En donde la clave identifica a la edad y el valor al nombre
+I. Imprima la cantidad de elementos del diccionario
+
+"""
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+print len(edades)
+

5aaff9179724b0167b8ba569a3db0a1e429456cf
jose guillermo <guillox22@gmail.com>
2015-04-09 12:22:11 -0300

    tp1ej15 v0.1
    2015(python 2.X)/practica 1/ejercicio 15

---
+++ Practica/2015/tp1/tp1ej15.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+__author__ = 'guillermo'
+
+"""15. Imprimir las horas que hay en 54980 minutos. ¿Cómo lo haría en Python 2.x y cómo en
+Python 3.x? ¿Qué cambia?
+
+Python 2.x
+print 'la cantidad de horas que hay en 54980 minutos es:', 54980//60
+>>> 916
+print 54980/60
+>>> 916
+
+Python 3.x
+print ('la cantidad de horas que hay en 54980 minutos es:', 54980//60)
+>>>916
+print (54980/60)
+>>>916,333333333337
+
+cuando aplicamos la division / en python 2 me lo redondea en cambio en python 3 no
+"""
+
+print 'la cantidad de horas que hay en 54980 minutos es:', 54980//60
+print 54980/60

f7fc3861ab799fbd5951d17313b9dce0bb002cdf
jose guillermo <guillox22@gmail.com>
2015-04-09 02:23:34 -0300

    tp1ej15bix v0.1

---
+++ Practica/2014/tp1/tp1ej15bix.py
@@ -0,0 +1,13 @@
+"""
+15.- Diccionarios
+b) Sea el siguiente diccionario:
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+En donde la clave identifica a la edad y el valor al nombre
+edades.clear()
+
+
+IX. Elimine todos los elementos del diccionario
+"""
+
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+print edades.clear()

d7cd53a240f06eda1a1fcf18ab6f0d3044232e05
jose guillermo <guillox22@gmail.com>
2015-04-09 11:14:09 -0300

    tp2ej8cola v0.1

---
+++ Practica/2014/tp2/tp2ej8cola.py
@@ -0,0 +1,22 @@
+"""
+Módulos
+8.- Escriba cada uno de los incisos del ejercicio 7.- (a, b y c) en módulos diferentes (conjuntos.py, cola.py,
+parametros_variables.py), impórtelos en un nuevo módulo (principal.py) y utilice sus funciones en éste último.
+cola.py
+"""
+
+def fcolapush(listapush,lista[i]):
+	return listapush.append(lista[i])
+
+def fcolapop(listapop,lista):
+	for i in range(-len(lista),0)
+		  listapop.pop(i)	
+
+
+
+lista=[22,'casa',"una lista",[1,2]]
+listapush=[]
+listapop=[]
+for i in range(1,len(lista)):
+	fcolapush(listapush,lista[i])
+fcolapop(listapop,lista)	

9b0bd4a83ebd1e6657b2170b336a7807a67c8eb8
jose guillermo <guillox22@gmail.com>
2015-04-09 02:08:22 -0300

    tp1ej7 v0.1

---
+++ Practica/2014/tp1/tp1ej7.py
@@ -0,0 +1,10 @@
+#7.- Escriba un script que reciba desde la entrada de datos estándar (teclado) una letra o número
+
+#y lo devuelva de la siguiente manera:
+
+#Ayuda: investigue el comando input()
+
+#print("El caracter ingresado es: " + caracter)"""
+
+caracter = input ("Ingrese cantidad:")
+print("El caracter ingresado es: " + caracter)

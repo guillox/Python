22b0b4e6e6907855235f74ef6b3c2a7379b17526
jose guillermo <guillox22@gmail.com>
2015-04-09 02:22:35 -0300

    tp1ej15biii v0.1

---
+++ Practica/2014/tp1/tp1ej15biii.py
@@ -0,0 +1,22 @@
+"""15.- Diccionarios
+b) Sea el siguiente diccionario:
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+En donde la clave identifica a la edad y el valor al nombre
+
+III. Imprima todas las edades de las personas del diccionario
+
+ejemplo
+keys() Devuelve una lista de las claves del diccionario.
+
+python 2.x
+
+>>>diccionario.keys()
+['enero', 'marzo', 'febero']
+
+python 3.x
+
+>>>list(diccionario.keys())
+['enero', 'marzo', 'febero']
+"""
+edades = {25:'Juan',33: 'Flor',17:'Pedro',40:'Alberto'}
+print edades.keys()

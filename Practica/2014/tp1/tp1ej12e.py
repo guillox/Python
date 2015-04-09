2bebbb9c8d64148bc5e57a396d222e9cb2490601
jose guillermo <guillox22@gmail.com>
2015-04-09 02:14:39 -0300

    tp1ej12e v0.1

---
+++ Practica/2014/tp1/tp1ej12e.py
@@ -0,0 +1,11 @@
+""" 
+Listas, Tuplas y Diccionarios
+12.- Listas
+Dada la siguiente lista:
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+
+
+e) Imprima ahora los últimos 3 elementos (sin saber a priori cuantos tiene la lista)
+"""
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+print lista[-3:]

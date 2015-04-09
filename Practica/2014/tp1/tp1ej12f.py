21f13efaf8ddda35619c9fdb4a6b2468974c5269
jose guillermo <guillox22@gmail.com>
2015-04-09 02:15:01 -0300

    tp1ej12f v0.1

---
+++ Practica/2014/tp1/tp1ej12f.py
@@ -0,0 +1,11 @@
+"""
+Listas, Tuplas y Diccionarios
+12.- Listas
+Dada la siguiente lista:
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+f) En la segunda posición ingrese el valor “elemento 2” """
+
+
+lista = [ 'elemento1' , 2 ,'elemento 3',   'elemento 4' ]
+lista.insert(1, "elemento 2")
+print lista[:]

eecb7fe458c4ebd122022544631e32754c5373d2
jose guillermo <guillox22@gmail.com>
2015-04-09 12:22:31 -0300

    tp1ej16 v0.1
    2015(python 2.X)/practica 1/ejercicio 16

---
+++ Practica/2015/tp1/tp1ej16.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+__author__ = 'guillermo'
+"""16. Ingresar el largo, ancho y profundidad de una pileta e imprimir la cantidad de litros que
+necesita para llenarse."""
+
+largo = int(raw_input('Ingrese el largo de la pileta:'))
+ancho = int(raw_input('Ingrese el ancho de la pileta:'))
+profundidad = int(raw_input('Ingrese la profundidad de una pileta:'))
+print 'la cantidad de litros que necesita para llenarse es:', largo * ancho * profundidad

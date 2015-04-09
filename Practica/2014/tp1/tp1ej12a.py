1e207b2dd0d8b5d366eac4b11982d6d68d4532e8
jose guillermo <guillox22@gmail.com>
2015-04-09 02:13:18 -0300

    tp1ej12a v0.1

---
+++ Practica/2014/tp1/tp1ej12a.py
@@ -0,0 +1,10 @@
+"""Listas, Tuplas y Diccionarios
+12.- Listas
+Dada la siguiente lista:
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+a) Imprima  el primer y último elemento """
+
+lista = [ 'elemento1' , 2 ,   'elemento 3',   'elemento 4' ]
+print lista[1]
+print lista[3]
+

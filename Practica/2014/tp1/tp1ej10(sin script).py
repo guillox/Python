792119cd2710f79e84dd3c597de7b5e2feb4398a
jose guillermo <guillox22@gmail.com>
2015-04-09 02:09:59 -0300

    tp1ej10 v0.1
    sin script

---
+++ Practica/2014/tp1/tp1ej10(sin script).py
@@ -0,0 +1,9 @@
+""" a) Escriba un script que reciba un carácter por entrada estándar e imprima su                          
+correspondiente código ASCII en pantalla.
+>>> ord('B')
+66
+
+"""
+
+caracter=raw_imput('escriba un caracter')
+print ord(caracter)

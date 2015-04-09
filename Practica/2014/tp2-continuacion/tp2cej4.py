978c93ea767d93e7be6ef3050e2e0228b5546830
jose guillermo <guillox22@gmail.com>
2015-04-09 11:27:20 -0300

    tp2cej4 v0.1

---
+++ Practica/2014/tp2-continuacion/tp2cej4.py
@@ -0,0 +1,6 @@
+"""
+4.­ Escriba un módulo que provea la funcionalidad de almacenar en un archivo nombres de                              
+personas y sus edades. Además realice una función que busque en el archivo e imprima un                                
+listado de todos los nombres cuya edad sea mayor a un número determinado.
+
+"""

259099d076a1308aac24744c902f37dcb609ee31
jose guillermo <guillox22@gmail.com>
2015-04-09 12:12:14 -0300

    tp1ej06 v0.1
    practica 1 ejercicio 1(2015)

---
+++ Practica/2015/tp1/tp1ej06.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+__author__ = 'guillermo marti'
+"""6. ¿El tipo entero tiene algún límite de precisión? ¿Si es así, cuál es el mayor número que
+permite representar? ¿Qué sucede cuando desborda el límite? Pruebe con un ejemplo
+en la consola"""
+
+num = int(raw_input("Ingresa un entero, por favor: "))
+print(num)
